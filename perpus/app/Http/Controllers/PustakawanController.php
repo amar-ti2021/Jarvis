0ffc2a24b75e670971759e969203a4eea6664b8a
amar-ti2021 <muha21095ti@student.nurulfikri.ac.id>
2022-10-29 19:07:24 +0700

    Create Pustakawan Controller

--- perpus/app/Http/Controllers/PustakawanController.php
+++ perpus/app/Http/Controllers/PustakawanController.php
@@ -2,16 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Librarian;
 use Illuminate\Http\Request;
 
 class PustakawanController extends Controller
 {
     public function index(){
-        $data = [
-            'nama' => 'Amar',
-            'gender' => 'L',
-            'shift' => 'malam'
-        ];
-        return response()->json($data);
+        $data = Librarian::all();
+        
+        if(count($data) == 0){
+            return response()->json([
+                'message' => "Get All Resource",
+                'status' => 204,
+            ], 204);
+        }
+
+        return response()->json([
+            'message' => "Get All Resource",
+            'status' => 200,
+            'data' => $data
+        ], 200);
     }
 }
