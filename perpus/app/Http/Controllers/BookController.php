ca66b2ad4886a18bc4932618eaed948c0d3e0e95
amar-ti2021 <muha21095ti@student.nurulfikri.ac.id>
2022-11-05 12:19:25 +0700

    Add new functions store and show

--- perpus/app/Http/Controllers/BookController.php
+++ perpus/app/Http/Controllers/BookController.php
@@ -7,10 +7,11 @@ use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $books = Book::all();
 
-        if(count($books) == 0){
+        if (count($books) == 0) {
             return response()->json([
                 'message' => "Get All Resource",
                 'status' => 204,
@@ -21,6 +22,38 @@ class BookController extends Controller
             'message' => "Get All Resource",
             'status' => 200,
             'data' => $books
+        ], 200);
+    }
+    public function store(Request $request)
+    {
+        $created = Book::create([
+            "title" => $request->title,
+            "author" => $request->author,
+            "publisher" => $request->publisher,
+            "rating" => $request->rating,
+            "published_date" => $request->published_date
+        ]);
+        return response()->json([
+            "message" => "Resources created successfully",
+            "status" => 201,
+            'data' => $created
+        ], 201);
+    }
+
+    public function show($id)
+    {
+        $book = Book::find($id);
+
+        if (!$book) {
+            return response()->json([
+                'message' => "Resource Not Found",
+                'status' => 404,
+            ], 404);
+        }
+        return response()->json([
+            'message' => "Get detail Resource",
+            'status' => 200,
+            'data' => $book
         ], 200);
     }
 }
